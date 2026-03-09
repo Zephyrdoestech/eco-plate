ad38f4407e23b0fcc97a84d3d462c0a076a3048c
LAB24_USER ON WS18 <L24X15W18@LabHCI.net>
2026-03-09 17:29:24 +0800

    Made some small tweaks

--- app/src/main/java/com/example/ecoplate/LoginActivity.kt
+++ app/src/main/java/com/example/ecoplate/LoginActivity.kt
@@ -1,11 +1,56 @@
 package com.example.ecoplate
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class LoginActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+//        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+//        val textviewRegisterLink = findViewById<TextView>(R.id.textviewRegisterLink)
+//        val edittextUsername = findViewById<EditText>(R.id.edittextUsername)
+//
+//        buttonLogin.setOnClickListener {
+//            val typedUsername = edittextUsername.text.toString()
+//
+//            val intentToDashboard = Intent(this, DashboardActivity::class.java)
+//
+//            intentToDashboard.putExtra("EXTRA_USERNAME", typedUsername)
+//
+//            startActivity(intentToDashboard)
+//        }
+//
+//        textviewRegisterLink.setOnClickListener {
+//            val intent = Intent(this, RegisterActivity::class.java)
+//            startActivity(intent)
+//        }
+
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+        val textviewRegisterLink = findViewById<TextView>(R.id.textviewRegisterLink)
+        val edittextUsername = findViewById<EditText>(R.id.edittextUsername)
+        val edittextPassword = findViewById<EditText>(R.id.edittextPassword)
+
+        val passedUser = intent.getStringExtra("PASS_USERNAME")
+        val passedPass = intent.getStringExtra("PASS_PASSWORD")
+        if(passedUser != null) edittextUsername.setText(passedUser)
+        if(passedPass != null) edittextPassword.setText(passedPass)
+
+        buttonLogin.setOnClickListener {
+            val intent = Intent(this@LoginActivity, DashboardActivity::class.java)
+            intent.putExtra("PASS_USERNAME", edittextUsername.text.toString())
+            intent.putExtra("PASS_PASSWORD", edittextPassword.text.toString())
+            startActivity(intent)
+        }
+
+        textviewRegisterLink.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/ecoplate/RegisterActivity.kt
+++ app/src/main/java/com/example/ecoplate/RegisterActivity.kt
@@ -1,15 +1,52 @@
 package com.example.ecoplate
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class RegisterActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+//        val buttonSubmit = findViewById<Button>(R.id.buttonSubmit)
+//
+//        buttonSubmit.setOnClickListener {
+//            val intentToLogin = Intent(this, LoginActivity::class.java)
+//            startActivity(intentToLogin)
+//            finish()
+//        }
+
+
+        val edittextUsername = findViewById<EditText>(R.id.edittextUsername)
+        val edittextPassword = findViewById<EditText>(R.id.edittextPassword)
+        val buttonSubmit = findViewById<Button>(R.id.buttonSubmit)
+
+        val edittextRePassword = findViewById<EditText>(R.id.edittextRePassword)
+
+        buttonSubmit.setOnClickListener {
+            val usernameStr = edittextUsername.text.toString()
+            val passwordStr = edittextPassword.text.toString()
+            val rePasswordStr = edittextRePassword.text.toString()
+
+            if(passwordStr == rePasswordStr && passwordStr.isNotEmpty()){
+                val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+
+                intent.putExtra("PASS_USERNAME", edittextUsername.text.toString())
+                intent.putExtra("PASS_PASSWORD", edittextPassword.text.toString())
+
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(this, "Passwords do not match!", Toast.LENGTH_SHORT).show()
+                edittextRePassword.error = "Passwords must match"
+            }
+
+        }
+
     }
 }
